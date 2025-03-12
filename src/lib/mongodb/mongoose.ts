6752dcb5680380ae60d20776d9d3a50a38cbcc94
jeenababujim <jeenababuji@gmail.com>
2025-03-12 17:51:26 +0530

    create user function fields changed

--- src/lib/mongodb/mongoose.ts
+++ src/lib/mongodb/mongoose.ts
@@ -1,23 +1,25 @@
 import mongoose from "mongoose";
 
-let initialized = false;
-
 export const connect = async () => {
-    mongoose.set('strictQuery', true);
-    
-    if (initialized) {
-        console.log('Mongodb is already connected');
+    mongoose.set("strictQuery", true);
+
+    if (mongoose.connection.readyState >= 1) {
+        console.log(" MongoDB is already connected");
         return;
     }
 
+    if (!process.env.MONGODB_URI) {
+        throw new Error(" Error: MONGODB_URI is not defined in .env file");
+    }
+
     try {
-        await mongoose.connect(process.env.MONGODB_URI as string, {
-            dbName: 'next-estate',
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "next-estate",
         });
 
-        console.log('Connected to MongoDB');
-        initialized = true;
+        console.log(" Connected to MongoDB");
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error(" Error connecting to MongoDB:", error);
+        throw error;
     }
 };
