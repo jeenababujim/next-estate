d5ba506f9fc0f71ee8e75dd488a560f27c5c42ae
jeenababujim <jeenababuji@gmail.com>
2025-03-24 18:17:30 +0530

    listing detail page modified

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking } from 'react-icons/fa';
 
 
 
@@ -50,7 +51,7 @@ export default async function Listing({ params }: { params: Promise<{ id: string
       </main>
     );
   }
-
+if(listing && listing.name){
   return (
     <main className="p-3 max-w-6xl mx-auto">
       <div className="relative w-full h-96">
@@ -69,6 +70,66 @@ export default async function Listing({ params }: { params: Promise<{ id: string
         )}
       </div>
       <h1 className="text-2xl font-bold mt-4">{listing.name || 'No Title'}</h1>
+      <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+      <p className="text-2xl font-semibold">
+        {listing?.name} - $
+        {listing?.offer
+          ? listing?.discountedPrice?.toLocaleString("en-US") || "N/A"
+          : listing?.regularPrice?.toLocaleString("en-US") || "N/A"}
+        {listing?.Type === "rent" && " / month"}
+      </p>
+      <p className='flex items-center mt-6 gap-2 text-slate-600 text-sm '>
+        <FaMapMarkerAlt className='text-green-700'/>
+        {listing.address}
+      </p>
+      <div className='flex-gap-4'>
+     <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md' >
+       {listing.type==='rent'?'For Rent':'For Sale'}
+     </p>
+     {listing.offer && (
+      <p className='bg-blue-900 w-full max-w-[200px] text-white text-center p-1 rounded-md' >
+       ${+listing.regularPrice} - {+listing.discountedPrice }  OFF
+      </p>
+     )}
+      </div>
+      <p className='text-slate-800'>
+       <span className='font-semibold text-black'>
+       Description -
+       {listing.description}
+       </span>
+      </p>
+      <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+      <li className='flex items-center gap-1 whitespace-nowrap'>
+        <FaBed className='text-lg'/>
+        {listing.bedrooms>1
+        ? `${listing.bedrooms} beds`
+        : `${listing.bedrooms} bed`
+        }
+      </li>
+      <li className='flex items-center gap-1 whitespace-nowrap'>
+        <FaBath className='text-lg'/>
+        {listing.bathrooms>1
+        ? `${listing.bathrooms} baths`
+        : `${listing.bathrooms} bath`
+        }
+      </li>
+      <li className='flex items-center gap-1 whitespace-nowrap'>
+        <FaParking className='text-lg'/>
+        {listing.parking
+        ? `Parking spot`
+        : `No Parking`
+        }
+      </li>
+      <li className='flex items-center gap-1 whitespace-nowrap'>
+        <FaChair className='text-lg'/>
+        {listing.parking
+        ? `Furnished`
+        : `Unfurnished`
+        }
+      </li>
+      </ul>
+      </div>
     </main>
   );
+}
 }
